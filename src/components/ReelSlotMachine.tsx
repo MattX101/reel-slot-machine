b68d9aaa4ce8b89da20563ba6b3e8beecae2346a
MattX101 <matthew.xuereb@protonmail.com>
2024-06-08 15:19:21 +0200

    Generating a random value on spin

--- src/components/ReelSlotMachine.tsx
+++ src/components/ReelSlotMachine.tsx
@@ -1,30 +1,37 @@
+import { Component } from 'react';
 import ReelSpinButton from './ReelSpinButton'
 
-const ReelSlotMachine = ({ }) => {
-    return (
-        <>
-            <div className='text-6xl font-extrabold '>
-                <div className="grid grid-cols-3 grid-rows-3 gap-2 bg-slate-400 p-2.5p rounded-t-xl">
-                    {/* Left Column */}
-                    <div className='grid h-24 col-start-1 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center'>1</div>
-                    <div className="grid col-start-1 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">2</div>
-                    <div className="grid col-start-1 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">3</div>
+class ReelSlotMachine extends Component {
+    render() {
+        const leftColId   = "leftCol";
+        const middleColId = "middleCol";
+        const rightColId  = "rightCol";
 
-                    {/* Middle Column */}
-                    <div className="grid col-start-2 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center">1</div>
-                    <div className="grid col-start-2 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">2</div>
-                    <div className="grid col-start-2 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">3</div>
+        return (
+            <>
+                <div className='text-6xl font-extrabold'>
+                    <div className="grid grid-cols-3 grid-rows-3 gap-2 bg-slate-400 p-2.5p rounded-t-xl">
+                        {/* Left Column */}
+                        <div id={leftColId + 1} className='grid h-24 col-start-1 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center'>1</div>
+                        <div id={leftColId + 2} className="grid col-start-1 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">2</div>
+                        <div id={leftColId + 3} className="grid col-start-1 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">3</div>
 
-                    {/* Right Column */}
-                    <div className="grid col-start-3 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center">1 </div>
-                    <div className="grid col-start-3 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">2</div>
-                    <div className="grid col-start-3 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">3</div>
+                        {/* Middle Column */}
+                        <div id={middleColId + 1} className="grid col-start-2 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center">1</div>
+                        <div id={middleColId + 2} className="grid col-start-2 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">2</div>
+                        <div id={middleColId + 3} className="grid col-start-2 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">3</div>
+
+                        {/* Right Column */}
+                        <div id={rightColId + 1} className="grid col-start-3 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center">1 </div>
+                        <div id={rightColId + 2} className="grid col-start-3 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">2</div>
+                        <div id={rightColId + 3} className="grid col-start-3 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">3</div>
+                    </div>
                 </div>
-            </div>
 
-            <ReelSpinButton />
-        </>
-    )
+                <ReelSpinButton ids={[leftColId, middleColId, rightColId]} />
+            </>
+        )
+    }
 }
 
 export default ReelSlotMachine;
--- src/components/ReelSpinButton.tsx
+++ src/components/ReelSpinButton.tsx
@@ -1,11 +1,43 @@
-const ReelSpinButton = ({ }) => {
+import React from "react";
+
+type Props = {
+    ids: Array<string>;
+}
+
+const ReelSpinButton: React.FC<Props> = ({ ids }) => {
+    const min = 0;
+    const max = 9;
+
+    function generateRadomNumber() {
+        return Math.ceil(Math.random() * max);
+    }
+
+    function setElement(id: string, value: number) {
+        let display = document.getElementById(id) as HTMLElement;
+        display.innerText = value.toString();
+    }
+
+    function setReel(id: string) {
+        const value = generateRadomNumber();
+
+        setElement(id + 1, value === min ? max : value - 1);
+        setElement(id + 2, value);
+        setElement(id + 3, value === max ? min : value + 1);
+    }
+
+    function spin() {
+        setReel(ids[0]);
+        setReel(ids[1]);
+        setReel(ids[2]);
+    }
+
     return (
-        <div className=' bg-slate-400 p-2.5p grid place-items-center rounded-b-xl'>
-            <button className="text-2xl font-bold bg-red-600 rounded-md text-slate-800 p-1p px-5p hover:bg-red-300 ">
+        <div className='bg-slate-400 p-2.5p grid place-items-center rounded-b-xl'>
+            <button onClick={() => spin()} className="text-2xl font-bold bg-red-600 rounded-md text-slate-800 p-1p px-5p hover:bg-red-300">
                 Spin
             </button>
         </div>
     )
 }
 
-export default ReelSpinButton;
+export default ReelSpinButton
