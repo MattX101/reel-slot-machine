2dd0cab7fbbac2e2d576e22fcfd93e34cc6d29da
MattX101 <matthew.xuereb@protonmail.com>
2024-06-09 16:10:58 +0200

    Added comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import ReelSlotMachine from './components/ReelSlotMachine';
 function App() {
   return (
     <>
+      {/* Background */}
       <div className='grid h-screen bg-blue-200 px-2.5p py-1p place-items-center'>
+        {/* Contents */}
         <div className='bg-sky-100 border-2 border-blue-300 rounded-xl p-2.5p w-90p'>
+          
+          {/* Title */}
+          <h1 className='mb-5 font-serif text-4xl italic font-bold text-center text-sky-800 '>
+            Reel Slot Machine
+          </h1>
 
-          <div className=' px-15p'>
-
-            <h1 className='mb-5 font-serif text-4xl italic font-bold text-center text-sky-800 '>
-              Reel Slot Machine
-            </h1>
-
-            <ReelSlotMachine />
-          </div>
+          {/* Slot Machine */}
+          <ReelSlotMachine />
         </div>
 
       </div>
--- src/components/ReelSpinButton.tsx
+++ src/components/ReelSpinButton.tsx
@@ -17,6 +17,7 @@ type Props = {
 }
 
 const ReelSpinButton: React.FC<Props> = ({ ids }) => {
+    // An array of images used for the slot machine values
     const logos = [
         ZeroLogo,
         OneLogo,
@@ -33,38 +34,52 @@ const ReelSpinButton: React.FC<Props> = ({ ids }) => {
     const min = 0;
     const max = logos.length - 1;
 
+    /** Returns an integer number between 0 and max, used to set the src of the slot images*/
     function generateRadomNumber() {
-        return Math.ceil(Math.random() * max);
+        // An integer number between 0 and max is needed for...
+        // a valid index to be used in the logos array, however...
+        // Math.random only generates a linear decimal number (0 to 1).
+
+        // On number generation the value is multiplied by max to return a number between 0 and max,...
+        // Math.ceil is used to set the decimal number to an integer.
+
+        // The reason for the +/- 1 is due to ceil function never returning a 0...
+        // due to the Math.random never generating a number smaller then 0
+
+        return Math.ceil(Math.random() * (max + 1)) - 1;
     }
 
-    function pickLogo(image: HTMLImageElement, value: number) {
-        if (value < 0 || value > logos.length) {
-            image.src = BlankLogo;
-            return;
-        }
+    /** Returns image logo for slot image */
+    function pickLogo(index: number) {
+        // If index outside of valid range, a blank logo is returned
+        if (index < 0 || index > logos.length)
+            return BlankLogo;
 
-        image.src = logos[value];
+        // Returns a valid logo
+        return logos[index];
     }
 
-    function setImage(id: string, value: number) {
-        pickLogo(
-            document.getElementById(id) as HTMLImageElement,
-            value
-        );
+    /** Sets image element through id */
+    function setImage(id: string, index: number) {
+        let image = document.getElementById(id) as HTMLImageElement;
+        image.src = pickLogo(index);
     }
 
-    function setReel(id: string) {
-        const value = generateRadomNumber();
+    /** Sets display column */
+    function setColumn(id: string) {
+        // RNG number used to set the image logos
+        const index = generateRadomNumber();
 
-        setImage(id + 1, value === min ? max : value - 1);
-        setImage(id + 2, value);
-        setImage(id + 3, value === max ? min : value + 1);
+        setImage(id + 1, index === min ? max : index - 1);
+        setImage(id + 2, index);
+        setImage(id + 3, index === max ? min : index + 1);
     }
 
+    /** Plays reel slot machine */
     function spin() {
-        setReel(ids[0]);
-        setReel(ids[1]);
-        setReel(ids[2]);
+        setColumn(ids[0]);
+        setColumn(ids[1]);
+        setColumn(ids[2]);
     }
 
     return (
