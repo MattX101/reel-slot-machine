d199f0dbfa7d112ba5479735c7c00adb2b04f3d6
MattX101 <matthew.xuereb@protonmail.com>
2024-06-10 10:29:24 +0200

    Improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ function App() {
   return (
     <>
       {/* Background */}
-      <div className='grid h-screen bg-blue-200 px-2.5p py-1p place-items-center'>
+      <div className='grid place-items-center h-screen py-1p'>
         {/* Contents */}
-        <div className='bg-sky-100 border-2 border-blue-300 rounded-xl p-2.5p w-90p'>
+        <div className='bg-sky-100 border-2 border-blue-300 rounded-xl p-2.5p'>
           
           {/* Title */}
-          <h1 className='mb-5 font-serif text-4xl italic font-bold text-center text-sky-800 '>
+          <h1 className='mb-5 font-serif text-4xl italic font-bold text-center text-sky-800'>
             Reel Slot Machine
           </h1>
 
           {/* Slot Machine */}
           <ReelSlotMachine />
+          
         </div>
 
       </div>
--- src/components/ReelSlotMachine.tsx
+++ src/components/ReelSlotMachine.tsx
@@ -12,45 +12,47 @@ class ReelSlotMachine extends Component {
 
         return (
             <>
+
                 {/* Slot Display */}
-                <div className="grid grid-cols-3 grid-rows-3 gap-2 bg-slate-400 p-2.5p rounded-t-xl">
+                <div className="grid grid-cols-3 grid-rows-3 gap-x-2 p-2.5p rounded-t-xl bg-slate-400">
 
                     {/* Left Column */}
-                    <div className='grid h-24 col-start-1 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center'>
-                        <img id={leftColId + 1} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className='grid col-start-1 row-start-1 rounded-t-full bg-slate-200'>
+                        <img id={leftColId + 1} className='h-24 p-15p' src={BlankLogo} alt='number' />
                     </div>
-                    <div className="grid col-start-1 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">
-                        <img id={leftColId + 2} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-1 row-start-2 bg-slate-200">
+                        <img id={leftColId + 2} className='h-24 p-5p' src={BlankLogo} alt='number' />
                     </div>
-                    <div className="grid col-start-1 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">
-                        <img id={leftColId + 3} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-1 row-start-3 rounded-b-full bg-slate-200">
+                        <img id={leftColId + 3} className='h-24 p-15p' src={BlankLogo} alt='number' />
                     </div>
 
                     {/* Middle Column */}
-                    <div className="grid col-start-2 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center">
-                        <img id={middleColId + 1} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-2 row-start-1 rounded-t-full bg-slate-200">
+                        <img id={middleColId + 1} className='h-24 p-15p' src={BlankLogo} alt='number' />
                     </div>
-                    <div className="grid col-start-2 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">
-                        <img id={middleColId + 2} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-2 row-start-2 bg-slate-200">
+                        <img id={middleColId + 2} className='h-24 p-5p' src={BlankLogo} alt='number' />
                     </div>
-                    <div className="grid col-start-2 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">
-                        <img id={middleColId + 3} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-2 row-start-3 rounded-b-full bg-slate-200">
+                        <img id={middleColId + 3} className='h-24 p-15p' src={BlankLogo} alt='number' />
                     </div>
 
                     {/* Right Column */}
-                    <div className="grid col-start-3 row-start-1 rounded-sm text-zinc-500 bg-slate-300 place-items-center">
-                        <img id={rightColId + 1} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-3 row-start-1 rounded-t-full bg-slate-200">
+                        <img id={rightColId + 1} className='h-24 p-15p' src={BlankLogo} alt='number' />
                     </div>
-                    <div className="grid col-start-3 row-start-2 rounded-sm text-zinc-700 bg-slate-200 place-items-center">
-                        <img id={rightColId + 2} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-3 row-start-2 bg-slate-200">
+                        <img id={rightColId + 2} className='h-24 p-5p' src={BlankLogo} alt='number' />
                     </div>
-                    <div className="grid col-start-3 row-start-3 rounded-sm text-zinc-500 bg-slate-300 place-items-center">
-                        <img id={rightColId + 3} className='h-24 p-2.5p' src={BlankLogo} alt='number' />
+                    <div className="grid col-start-3 row-start-3 rounded-b-full bg-slate-200">
+                        <img id={rightColId + 3} className='h-24 p-15p' src={BlankLogo} alt='number' />
                     </div>
 
                 </div>
 
                 <ReelSpinButton ids={[leftColId, middleColId, rightColId]} />
+
             </>
         )
     }
